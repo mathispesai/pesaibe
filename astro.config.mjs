58f9904df9eacab72497e39a363c2ea4c4bf4ee1
Mathis Bossuyt <mathis.bossuyt@ugent.be>
2025-08-01 21:27:15 +0200

    quick fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,12 @@
-// @ts-check
+// astro.config.ts
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
 
-import cloudflare from "@astrojs/cloudflare";
-
-import react from '@astrojs/react';
-
-// https://astro.build/config
 export default defineConfig({
   site: "https://www.pesai.be/",
   output: "static",
   integrations: [mdx(), sitemap(), react()],
-  adapter: cloudflare(),
+  // adapter: cloudflare(),  <-- deze lijn verwijderen
 });
